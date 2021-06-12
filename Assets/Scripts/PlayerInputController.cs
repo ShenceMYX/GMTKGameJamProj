92c79a183c2ae75f7aa478e851320892a4a85aa5
Shence <maoyuxuan129@gmail.com>
2021-06-12 18:51:46 +0800

    PlayerInputController Input detect

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -9,20 +9,76 @@ namespace Player
 	/// </summary>
 	public class PlayerInputController : MonoBehaviour
 	{
-        public PoleStatus UpPoleStatus { get; set; } = PoleStatus.None;
-        public PoleStatus DownPoleStatus { get; set; } = PoleStatus.None;
-        public PoleStatus LeftPoleStatus { get; set; } = PoleStatus.None;
-        public PoleStatus RightPoleStatus { get; set; } = PoleStatus.None;
+        public PoleStatus UpPoleStatus  = PoleStatus.None;
+        public PoleStatus DownPoleStatus  = PoleStatus.None;
+        public PoleStatus LeftPoleStatus  = PoleStatus.None;
+        public PoleStatus RightPoleStatus = PoleStatus.None;
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
-                if(UpPoleStatus == PoleStatus.NorthPole)
-                    UpPoleStatus = PoleStatus.None;
-                else
-                    UpPoleStatus = PoleStatus.NorthPole;
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    if (UpPoleStatus != PoleStatus.SouthPole)
+                        UpPoleStatus = PoleStatus.SouthPole;
+                    else
+                        UpPoleStatus = PoleStatus.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    if (DownPoleStatus != PoleStatus.SouthPole)
+                        DownPoleStatus = PoleStatus.SouthPole;
+                    else
+                        DownPoleStatus = PoleStatus.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.A))
+                {
+                    if (LeftPoleStatus != PoleStatus.SouthPole)
+                        LeftPoleStatus = PoleStatus.SouthPole;
+                    else
+                        LeftPoleStatus = PoleStatus.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.D))
+                {
+                    if (RightPoleStatus != PoleStatus.SouthPole)
+                        RightPoleStatus = PoleStatus.SouthPole;
+                    else
+                        RightPoleStatus = PoleStatus.None;
+                }
             }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    if (UpPoleStatus != PoleStatus.NorthPole)
+                        UpPoleStatus = PoleStatus.NorthPole;
+                    else
+                        UpPoleStatus = PoleStatus.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    if (DownPoleStatus != PoleStatus.NorthPole)
+                        DownPoleStatus = PoleStatus.NorthPole;
+                    else
+                        DownPoleStatus = PoleStatus.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.A))
+                {
+                    if (LeftPoleStatus != PoleStatus.NorthPole)
+                        LeftPoleStatus = PoleStatus.NorthPole;
+                    else
+                        LeftPoleStatus = PoleStatus.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.D))
+                {
+                    if (RightPoleStatus != PoleStatus.NorthPole)
+                        RightPoleStatus = PoleStatus.NorthPole;
+                    else
+                        RightPoleStatus = PoleStatus.None;
+                }
+            }
+           
         }
     }
 }
