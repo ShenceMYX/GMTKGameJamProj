8cd0098bd6923f4e93d6587779b15b56a33bd5f4
Shence <maoyuxuan129@gmail.com>
2021-06-12 17:47:32 +0800

    PlayerInputController

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 namespace ns
 {
 	/// <summary>
-	/// 
+	/// 玩家角色输入控制类
 	/// </summary>
 	public class PlayerInputController : MonoBehaviour
 	{
+        public PoleStatus UpPoleStatus { get; set; } = PoleStatus.None;
+        public PoleStatus DownPoleStatus { get; set; } = PoleStatus.None;
+        public PoleStatus LeftPoleStatus { get; set; } = PoleStatus.None;
+        public PoleStatus RightPoleStatus { get; set; } = PoleStatus.None;
 
-	}
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                if(UpPoleStatus == PoleStatus.NorthPole)
+                    UpPoleStatus = PoleStatus.None;
+                else
+                    UpPoleStatus = PoleStatus.NorthPole;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/PoleStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+	/// <summary>
+	/// 南北极状态枚举
+	/// </summary>
+	public enum PoleStatus
+	{
+		//北极
+		NorthPole,
+		//南极
+		SouthPole,
+		//啥也没有
+		None
+	}
+}
